d273ba4aca973ae57193091a3e327892a2d201d3
AngryFennec <shellfix@gmail.com>
2019-11-05 19:26:12 +0300

    Добавляет моки (все поломалось, но так надо)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/app/app.jsx';
+import films from './mocks/films.js';
 
 const init = () => {
-  const titles = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Yet another movie`];
   const onTitleClick = () => {};
 
   ReactDOM.render(
       <App
-        titles = {titles}
+        titles = {films}
         onTitleClick = {onTitleClick}
       />,
       document.querySelector(`#root`)
