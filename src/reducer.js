baae31538c377af3c677e3dccc8433c746e213bd
AngryFennec <shellfix@gmail.com>
2019-11-20 17:23:41 +0300

    Редьюсер

---
+++ src/reducer.js
@@ -0,0 +1,35 @@
+import films from "./mocks/films.js";
+
+const initialState = {
+  genre: DEFAULT_GENRE,
+  filteredFilms: []
+};
+
+const ActionCreator = {
+  applyGenreFilter: (genre) => ({
+    type: `CHANGE_FILTER`,
+    payload: genre
+  }),
+
+  getFilterefFilms: (films, genre) => ({
+    type: `FILTER_FILMS`,
+    payload: genre === `All genres` ? films : films.filter(({genre: filmGenre}) => genre === filmGenre);
+  })
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case `CHANGE_FILTER`:
+      return Object.assign({}, state, {
+        genre: action.payload
+      });
+    case `FILTER_FILMS`:
+      return Object.assign({}, state, {
+        filteredFilms: action.payload
+      });
+  }
+
+  return state;
+};
+
+export {ActionCreator, reducer};
